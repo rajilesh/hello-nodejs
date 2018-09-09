a6f207210c8686ea08cc1e17614a1bb9dc3e316d
Rajilesh Panoli <rajileshpanoli123@gmail.com>
2018-09-09 21:54:09 +0530

    config.js file

---
+++ config.js
@@ -0,0 +1,24 @@
+/*
+* Configuration variables
+*
+*/
+
+var environments = {};
+
+// Staging environment
+environments.staging = {
+    'port':3000,
+    'envName':'staging'
+}
+
+environments.production = {
+    'port':5000,
+    'envName':'production'
+
+}
+
+var currentEnvironment = typeof(process.env.NODE_ENV) =='string' ? process.env.NODE_ENV.toLowerCase() : '';
+
+var enviromentToExport = typeof(environments[currentEnvironment]) =='object' ? environments[currentEnvironment] : environments.staging;
+
+module.exports = enviromentToExport;
